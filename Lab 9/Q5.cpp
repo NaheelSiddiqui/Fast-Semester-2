28849af1a6a06003d6cbe20224f9cc385e80fa9b
NaheelSiddiqui <siddiquinaheel@gmail.com>
2025-04-22 01:08:22 +0500

    Update Q5.cpp

--- Lab 9/Q5.cpp
+++ Lab 9/Q5.cpp
@@ -2,37 +2,44 @@
 using namespace std;
 
 class Shape {
-    public:
-        double area;
-        Shape(double a): area(a) {}
-        virtual void getArea() = 0;
-        virtual ~Shape() {}
+public:
+    virtual void getArea() = 0;
+    virtual ~Shape() {}
 };
 
 class Rectangle : public Shape {
-    public:
-        Rectangle(double a): Shape(a) {}
-        void getArea() override{
-            cout << "Rectangle Area = " << area << endl;
-        }
+private:
+    double length, width;
+public:
+    Rectangle(double l, double w) : length(l), width(w) {}
+    void getArea() override {
+        double area = length * width;
+        cout << "Rectangle Area = " << area << endl;
+    }
 };
 
-class Triangle: public Shape {
-    public:
-        Triangle(double a): Shape(a) {}
-        void getArea() override{
-            cout << "Triangle Area = " << area << endl;
-        }
+class Triangle : public Shape {
+private:
+    double base, height;
+public:
+    Triangle(double b, double h) : base(b), height(h) {}
+    void getArea() override {
+        double area = 0.5 * base * height;
+        cout << "Triangle Area = " << area << endl;
+    }
 };
 
-int main(){
+int main() {
     Shape *s1, *s2;
 
-    s1 = new Rectangle(30.5);
-    s2 = new Triangle(20);
+    s1 = new Rectangle(5.0, 10.0);
+    s2 = new Triangle(4.0, 6.0);
 
     s1->getArea();
     s2->getArea();
+
+    delete s1;
+    delete s2;
 
     return 0;
 }
